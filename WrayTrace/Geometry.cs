a9cb9dfb9158082947cb50b1a8f0aa6e54a91d2b
willthamic <will.hamic@gmail.com>
2018-05-29 17:37:32 -0500

    Forgot to commit earlier. Added camera and sensor classes and fucked and implemented a incorrect cross product method in the geometry class.

--- WrayTrace/Geometry.cs
+++ WrayTrace/Geometry.cs
@@ -5,10 +5,10 @@ namespace Geometry
 {
     public class Rectangle
     {
-        Vector3 a;
-        Vector3 b;
-        Vector3 c;
-        Vector3 d;
+        public Vector3 a;
+        public Vector3 b;
+        public Vector3 c;
+        public Vector3 d;
 
         public Rectangle(Vector3 p1, Vector3 p2, Vector3 p3)
         {
@@ -17,6 +17,26 @@ namespace Geometry
             c = p3;
             d = a + c - b;
         }
+
+        public Vector3 Normal()
+        {
+            Vector3 u = b - a;
+            Vector3 v = d - a;
+
+            float u1 = u.x;
+            float u2 = u.y;
+            float u3 = u.z;
+            float v1 = v.x;
+            float v2 = v.y;
+            float v3 = v.z;
+
+            float x =   u.y*v.z-v.y*u.z;
+            float y = -(u.x*v.z-v.x-u.z);
+            float z =   u.x*v.y-v.x*u.y;
+            return new Vector3(x, y, z).Unit();
+        }
+
+
 
     }
 }
--- WrayTrace/Program.cs
+++ WrayTrace/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using Vector;
+using Geometry;
 
 namespace WrayTrace
 {
@@ -10,6 +13,53 @@ namespace WrayTrace
     {
         static void Main(string[] args)
         {
+            Bitmap image = new Bitmap(100, 100);
+
+            Vector3 a = new Vector3(-1, 1, 0);
+            Vector3 b = new Vector3(-1, -1, 0);
+            Vector3 c = new Vector3(1, -1, 0);
+
+            Geometry.Rectangle border = new Geometry.Rectangle(a, b, c);
+
+            Sensor sensor = new Sensor(border, 5, 5);
+
+            Console.WriteLine(border.Normal().ToString());
+        }
+    }
+
+    class Camera
+    {
+        Vector3 location;
+        //Vector3 direction;
+        Sensor sensor;
+
+        public Camera (Vector3 location0, Sensor sensor0)
+        {
+            location = location0;
+            //direction = direction0;
+            sensor = sensor0;
+        }
+    }
+
+    class Sensor
+    {
+        Geometry.Rectangle border;
+        int pWidth;
+        int pHeight;
+
+        public Sensor(Geometry.Rectangle border0, int pWidth0, int pHeight0)
+        {
+            border = border0;
+            pWidth = pWidth0;
+            pHeight = pHeight0;
+        }
+
+        public Vector3 LocatePixel(int x, int y)
+        {
+            Vector3 ad = border.d - border.a;
+            Vector3 ab = border.b - border.a;
+            Vector3 ap = (ad / (pWidth - 1) * x) + (ab / (pHeight - 1) * y);
+            return border.a + ap;
         }
     }
 }
