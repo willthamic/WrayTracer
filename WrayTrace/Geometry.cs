0b7e52df2bf3707dc681499459064bcb88eb94dd
willthamic <will.hamic@gmail.com>
2018-05-29 16:29:41 -0500

    added geometry class with basic functionality

---
+++ WrayTrace/Geometry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Vector;
+
+namespace Geometry
+{
+    public class Rectangle
+    {
+        Vector3 a;
+        Vector3 b;
+        Vector3 c;
+        Vector3 d;
+
+        public Rectangle(Vector3 p1, Vector3 p2, Vector3 p3)
+        {
+            a = p1;
+            b = p2;
+            c = p3;
+            d = a + c - b;
+        }
+
+    }
+}
