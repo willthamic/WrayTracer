9bfe1414d6a90bc3ec057c495b9a836a6ae62f8f
Will Hamic <will.hamic@gmail.com>
2018-07-06 03:08:58 -0500

    Didn't really change anything, just did massive reorganization and made it more expandable I guess.

--- WrayTrace/Geometry.cs
+++ WrayTrace/Geometry.cs
@@ -4,52 +4,22 @@ using Vector;
 
 namespace Geometry
 {
-    public class RayTriangle
+    public interface Element
     {
-        public static (bool, Vector3, float) W (Triangle triangle, Line line)
-        {
-            (bool, Vector3, float) raycast = Plane.RayCast(line, triangle.plane);
-            Vector3 p = raycast.Item2;
-            if (p == null)
-                return (false, null, 0);
-            float i = (float)Math.Acos(Vector3.Dot((triangle.a - p).Unit(), (triangle.b - p).Unit()));
-            float j = (float)Math.Acos(Vector3.Dot((triangle.b - p).Unit(), (triangle.c - p).Unit()));
-            float k = (float)Math.Acos(Vector3.Dot((triangle.c - p).Unit(), (triangle.a - p).Unit()));
-            if (i + j + k > 2 * Math.PI - 0.001 || i + j + k == float.NaN)
-                return (true, p, raycast.Item3);
-            else
-                return (false, null, 0);
-        }
-
-        public static (bool, Vector3, float) IO (Triangle triangle, Line line)
-        {
-            (bool, Vector3, float) raycast = Plane.RayCast(line, triangle.plane);
-            Vector3 p = raycast.Item2;
-            if (p == null)
-                return (false, null, 0);
-            Vector3 edgeA = triangle.b - triangle.a;
-            Vector3 edgeB = triangle.c - triangle.b;
-            Vector3 edgeC = triangle.a - triangle.c;
-            Vector3 ap = p - triangle.a;
-            Vector3 bp = p - triangle.b;
-            Vector3 cp = p - triangle.c;
-            if (Vector3.Dot(triangle.plane.normal, Vector3.Normal(edgeA, ap)) > 0 &&
-                Vector3.Dot(triangle.plane.normal, Vector3.Normal(edgeB, bp)) > 0 &&
-                Vector3.Dot(triangle.plane.normal, Vector3.Normal(edgeC, cp)) > 0)
-                return (true, p, raycast.Item3);
-            else
-                return (false, null, 0);
-        }
+        (bool, Vector3, float, Vector3) Raycast(Line line, String type);
     }
 
     /// <summary>
     /// Represents a triangle defined by three points in 3-dimensions.
     /// </summary>
-    public class Triangle
+    public class Triangle : Element
     {
         public Vector3 a;
         public Vector3 b;
         public Vector3 c;
+        public Vector3 ab;
+        public Vector3 bc;
+        public Vector3 ca;
         public Plane plane;
         
         public Triangle(Vector3 a0, Vector3 b0, Vector3 c0)
@@ -57,14 +27,64 @@ namespace Geometry
             a = a0;
             b = b0;
             c = c0;
+
+            ab = b - a;
+            bc = c - b;
+            ca = a - c;
+
             plane = new Plane(a, b - a, c - a);
+        }
+
+        public (bool, Vector3, float, Vector3) Raycast(Line line, String type)
+        {
+            return TriangleRaycast(this, line, type);
+        }
+
+        static (bool, Vector3, float, Vector3) TriangleRaycast(Triangle element, Line line, String type)
+        {
+            if (type == "IO")
+                return IO(element, line);
+            else
+                return W(element, line);
+        }
+
+        static (bool, Vector3, float, Vector3) W(Triangle triangle, Line line)
+        {
+            (bool, Vector3, float) raycast = Plane.RayCast(line, triangle.plane);
+            Vector3 p = raycast.Item2;
+            if (p == null)
+                return (false, null, 0, null);
+            float i = (float)Math.Acos(Vector3.Dot((triangle.a - p).Unit(), (triangle.b - p).Unit()));
+            float j = (float)Math.Acos(Vector3.Dot((triangle.b - p).Unit(), (triangle.c - p).Unit()));
+            float k = (float)Math.Acos(Vector3.Dot((triangle.c - p).Unit(), (triangle.a - p).Unit()));
+            if (i + j + k > 2 * Math.PI - 0.001 || i + j + k == float.NaN)
+                return (true, p, raycast.Item3, triangle.plane.normal);
+            else
+                return (false, null, 0, null);
+        }
+
+        static (bool, Vector3, float, Vector3) IO(Triangle triangle, Line line)
+        {
+            (bool, Vector3, float) raycast = Plane.RayCast(line, triangle.plane);
+            Vector3 p = raycast.Item2;
+            if (p == null)
+                return (false, null, 0, null);
+            Vector3 ap = p - triangle.a;
+            Vector3 bp = p - triangle.b;
+            Vector3 cp = p - triangle.c;
+            if (Vector3.Dot(triangle.plane.normal, Vector3.Normal(triangle.ab, ap)) > 0 &&
+                Vector3.Dot(triangle.plane.normal, Vector3.Normal(triangle.bc, bp)) > 0 &&
+                Vector3.Dot(triangle.plane.normal, Vector3.Normal(triangle.ca, cp)) > 0)
+                return (true, p, raycast.Item3, triangle.plane.normal);
+            else
+                return (false, null, 0, null);
         }
     }
 
     /// <summary>
     /// Represents a parallelogram defined by three points in 3-dimensions.
     /// </summary>
-    public class Parallelogram
+    public class Parallelogram : Element
     {
         public Triangle A;
         public Triangle B;
@@ -88,8 +108,6 @@ namespace Geometry
             B = new Triangle(a, c, d);
 
             plane = A.plane;
-
-
         }
 
         public Parallelogram Translate(Vector3 vector)
@@ -105,9 +123,18 @@ namespace Geometry
         {
             return parallelogram.Translate(vector);
         }
+
+        public (bool, Vector3, float, Vector3) Raycast(Line line, String type)
+        {
+            var temp = this.A.Raycast(line, type);
+            if (temp.Item1)
+                return temp;
+            else
+                return this.B.Raycast(line, type);
+        }
     }
 
-    public class Paralleloid
+    public class Paralleloid : Element
     {
         public Vector3[] points = new Vector3[8];
         public Parallelogram[] faces = new Parallelogram[6];
@@ -129,6 +156,18 @@ namespace Geometry
             faces[3] = faces[0] + (points[3] - points[0]);
             faces[4] = faces[1] + (points[1] - points[0]);
             faces[5] = faces[2] + (points[4] - points[0]);
+        }
+
+        public (bool, Vector3, float, Vector3) Raycast(Line line, String type)
+        {
+            (bool, Vector3, float, Vector3) intersect = (false, null, 0, null);
+            foreach (Parallelogram parallelogram in faces)
+            {
+                var temp = parallelogram.Raycast(line, type);
+                if (temp.Item1 && temp.Item3 > 0 && (!intersect.Item1 || temp.Item3 < intersect.Item3))
+                    intersect = temp;
+            }
+            return intersect;
         }
     }
 }
--- WrayTrace/Program.cs
+++ WrayTrace/Program.cs
@@ -23,30 +23,37 @@ namespace WrayTrace
             int width = 1920;
             int height = 1080;
 
-            String raycastMethod = "W";
+            String raycastMethod = "IO";
 
-            Bitmap bitmap = new Bitmap(width, height);
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    bitmap.SetPixel(x, y, Color.Black);
-                }
-            }
+            Bitmap image = new Bitmap(width, height);
+            float[,] imageValues = new float[width, height];
+            //for (int x = 0; x < width; x++)
+            //{
+            //    for (int y = 0; y < height; y++)
+            //    {
+            //        image.SetPixel(x, y, Color.Black);
+            //    }
+            //}
 
-            Parallelogram sensorBorder = new Parallelogram(V(-1, -4, 4), V(-1, -4, 2), V(1, -4, 2));
-            Sensor sensor = new Sensor(sensorBorder, width, height);
-            //Camera camera2 = new Camera(V(0, -5, 3), sensor);
-            Camera camera = new Camera(V(2, -4, 4)*1.5f, V(-.5f,1,-.75f).Unit(), 1.2f, width, height);
-
-            //Parallelogram rect1 = new Parallelogram(V(-1.5f, 1.5f, -1.5f), V(-1.5f, -1.5f, -1), V(1.5f, -1.5f, -1));
-            //Parallelogram rect2 = new Parallelogram(V(-.5f, .5f, -.7f), V(-.5f, -.5f, -.8f), V(.5f, -.5f, -.8f));
+            //Parallelogram sensorBorder = new Parallelogram(V(-1, -4, 4), V(-1, -4, 2), V(1, -4, 2));
+            //Sensor sensor = new Sensor(sensorBorder, width, height);
+            Camera camera = new Camera(V(2, -4, 4) * 1.5f, V(-.5f, 1, -.75f).Unit(), 1.2f, width, height);
+            //Camera camera = new Camera(V(0, 0, 2), V(0.1f, 0, -1), 1.2f, width, height);
 
             Parallelogram floor = new Parallelogram(V(-10, 10, 0), V(-10, -10, 0), V(10, -10, 0));
             Paralleloid cube = new Paralleloid(V(1, 1, 0), V(1, -1, 0), V(-1, 1, 0), V(1, 1, 2));
-            Paralleloid scube = new Paralleloid(V(0.866025404f, 0.5f, 2), V(0.5f, -0.866025404f, 2), V(-0.5f, 0.866025404f, 2), V(0.866025404f, 0.5f, 3.41f));
+            Paralleloid cube2 = new Paralleloid(V(0.866025404f, 0.5f, 2), V(0.5f, -0.866025404f, 2), V(-0.5f, 0.866025404f, 2), V(0.866025404f, 0.5f, 3.41f));
 
-            Console.Write("\rGenerating Scene [Done - {0}s]\r\n", 0.001 * stopwatch.ElapsedMilliseconds);
+            Light light1 = new Light(new Vector3(20, -14, 30), 5000);
+
+
+            List<Object> elements0 = new List<Object>();
+            elements0.Add(floor);
+            elements0.Add(cube);
+            elements0.Add(cube2);
+
+            List<Object> lights = new List<Object>();
+            lights.Add(light1);
 
             Triangle[] elements = {
                 floor.A, floor.B,
@@ -56,59 +63,57 @@ namespace WrayTrace
                 cube.faces[3].A, cube.faces[3].B,
                 cube.faces[4].A, cube.faces[4].B,
                 cube.faces[5].A, cube.faces[5].B,
-                scube.faces[0].A, scube.faces[0].B,
-                scube.faces[1].A, scube.faces[1].B,
-                scube.faces[2].A, scube.faces[2].B,
-                scube.faces[3].A, scube.faces[3].B,
-                scube.faces[4].A, scube.faces[4].B,
-                scube.faces[5].A, scube.faces[5].B
+                cube2.faces[0].A, cube2.faces[0].B,
+                cube2.faces[1].A, cube2.faces[1].B,
+                cube2.faces[2].A, cube2.faces[2].B,
+                cube2.faces[3].A, cube2.faces[3].B,
+                cube2.faces[4].A, cube2.faces[4].B,
+                cube2.faces[5].A, cube2.faces[5].B
             };
 
-            Light light = new Light(new Vector3(20, -14, 30), 5000);
+            Console.Write("\rGenerating Scene [Done - {0}s]\r\n", 0.001 * stopwatch.ElapsedMilliseconds);
 
-            for (var x = 0; x < width; x++)
+            for (int i = 0; i < width * height; i++)
             {
-                Console.Write("\rRendering Image [{0}%]", Math.Floor(100.0f * x / width));
-                for (var y = 0; y < height; y++)
+                if (i % width == 0)
+                    Console.Write("\rRendering Image [{0}%]", Math.Floor(100.0f * i / (width * height)));
+                int pixelX = i % width;
+                int pixelY = i / width;
+
+                Line ray = new Line(camera.location, camera.sensor.LocatePixel(pixelX, pixelY) - camera.location);
+
+                Object intersectedObject = null;
+                Vector3 p = null;
+                float minT = 0;
+                Vector3 surfaceNormal = null;
+
+                bool flag = true;
+
+                foreach (Element element in elements0)
                 {
-                    Line ray = new Line(camera.location, camera.sensor.LocatePixel(x, y) - camera.location);
-
-                    Triangle intersectedObject = null;
-                    Vector3 p = null;
-                    float minT = 0;
-
-                    bool flag = true;
-
-                    foreach (Triangle element in elements)
+                    var intersect = element.Raycast(ray, raycastMethod);
+                    if (intersect.Item1 && ((flag || intersect.Item3 < minT) && intersect.Item3 > 0))
                     {
-                        (bool, Vector3, float) intersect = (false, null, float.NaN);
-                        if (raycastMethod == "W")
-                            intersect = RayTriangle.W(element, ray);
-                        else if (raycastMethod == "IO")
-                            intersect = RayTriangle.IO(element, ray);
-
-                        if (intersect.Item1 && ((flag || intersect.Item3 < minT) && intersect.Item3 > 0))
-                        {
-                            flag = false;
-                            intersectedObject = element;
-                            p = intersect.Item2;
-                            minT = intersect.Item3;
-                        }
+                        flag = false;
+                        intersectedObject = element;
+                        p = intersect.Item2;
+                        minT = intersect.Item3;
+                        surfaceNormal = intersect.Item4;
                     }
 
-                    if (p != null)
+                }
+
+                if (intersectedObject != null)
+                {
+                    foreach (Light light in lights)
                     {
                         bool clearpath = true;
 
-                        foreach (Triangle element in elements)
+                        foreach (Element element in elements0)
                         {
                             if (element == intersectedObject)
                                 continue;
-                            (bool, Vector3, float) intersect = (false, null, float.NaN);
-                            if (raycastMethod == "W")
-                                intersect = RayTriangle.W(element, new Line(p, light.location - p));
-                            else if (raycastMethod == "IO")
-                                intersect = RayTriangle.IO(element, new Line(p, light.location - p));
+                            var intersect = element.Raycast(new Line(p, light.location - p), raycastMethod);
 
                             if (intersect.Item1 && intersect.Item3 > 0)
                             {
@@ -119,17 +124,89 @@ namespace WrayTrace
 
                         if (clearpath)
                         {
-                            int intensity = Convert.ToInt32(Vector3.Dot(intersectedObject.plane.normal.Unit(), (light.location - p).Unit()) * light.intensity / (light.location - p).Magnitude());
-                            bitmap.SetPixel(x, y, Color.FromArgb(intensity, intensity, intensity));
+                            imageValues[pixelX, pixelY] += light.GetIntensity(surfaceNormal, p);
                         }
                     }
                 }
             }
 
+            //for (var x = 0; x < width; x++)
+            //{
+            //    Console.Write("\rRendering Image [{0}%]", Math.Floor(100.0f * x / width));
+            //    for (var y = 0; y < height; y++)
+            //    {
+            //        Line ray = new Line(camera.location, camera.sensor.LocatePixel(x, y) - camera.location);
+
+            //        Triangle intersectedObject = null;
+            //        Vector3 p = null;
+            //        float minT = 0;
+
+            //        bool flag = true;
+
+            //        foreach (Triangle element in elements)
+            //        {
+            //            (bool, Vector3, float) intersect = (false, null, float.NaN);
+            //            if (raycastMethod == "W")
+            //                intersect = RayTriangle.W(element, ray);
+            //            else if (raycastMethod == "IO")
+            //                intersect = RayTriangle.IO(element, ray);
+
+            //            if (intersect.Item1 && ((flag || intersect.Item3 < minT) && intersect.Item3 > 0))
+            //            {
+            //                flag = false;
+            //                intersectedObject = element;
+            //                p = intersect.Item2;
+            //                minT = intersect.Item3;
+            //            }
+            //        }
+
+            //        if (p != null)
+            //        {
+            //            bool clearpath = true;
+
+            //            foreach (Triangle element in elements)
+            //            {
+            //                if (element == intersectedObject)
+            //                    continue;
+            //                (bool, Vector3, float) intersect = (false, null, float.NaN);
+            //                if (raycastMethod == "W")
+            //                    intersect = RayTriangle.W(element, new Line(p, light.location - p));
+            //                else if (raycastMethod == "IO")
+            //                    intersect = RayTriangle.IO(element, new Line(p, light.location - p));
+
+            //                if (intersect.Item1 && intersect.Item3 > 0)
+            //                {
+            //                    clearpath = false;
+            //                    break;
+            //                }
+            //            }
+
+            //            if (clearpath)
+            //            {
+            //                int intensity = Convert.ToInt32(Vector3.Dot(intersectedObject.plane.normal.Unit(), (light.location - p).Unit()) * light.intensity / (light.location - p).Magnitude());
+            //                image.SetPixel(x, y, Color.FromArgb(intensity, intensity, intensity));
+            //            }
+            //        }
+            //    }
+            //}
+
+            
+
             Console.Write("\rGenerating Image [Done - {0}s]\n\r", 0.001 * stopwatch.ElapsedMilliseconds);
             Console.Write("Saving Image...");
 
-            bitmap.Save("img.bmp");
+            float maxIntensity = (from float v in imageValues select v).Max();
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    int intensity = Convert.ToInt32(imageValues[x, y] / maxIntensity * 255);
+                    image.SetPixel(x, y, Color.FromArgb(intensity, intensity, intensity));
+                }
+            }
+
+            image.Save("img.bmp");
             Console.Write("\rSaving Image [Done - {0}s]\n\r", 0.001 * stopwatch.ElapsedMilliseconds);
         }
 
@@ -193,6 +270,31 @@ namespace WrayTrace
         {
             location = location0;
             intensity = intensity0;
+        }
+
+        public float GetIntensity (Vector3 surfaceNormal, Vector3 p)
+        {
+            return Vector3.Dot(surfaceNormal.Unit(), (location - p).Unit()) * intensity / (location - p).Magnitude();
+        }
+    }
+
+    class Spotlight : Light
+    {
+        float minAngle;
+        float maxAngle;
+        float minCosAngle;
+        float maxCosAngle;
+
+        public Spotlight(Vector3 location0, float intensity0, float angle) : this(location0, intensity0, angle, angle) {}
+
+        public Spotlight(Vector3 location0, float intensity0, float minAngle0, float maxAngle0) : base(location0, intensity0)
+        {
+            location = location0;
+            intensity = intensity0;
+            minAngle = minAngle0;
+            maxAngle = maxAngle0;
+            minCosAngle = (float) Math.Cos(minAngle);
+            maxCosAngle = (float) Math.Cos(maxAngle);
         }
     }
 }
